8b0c0a1e47e3ac8de968aae889943d79040fa8d7
JustinBenito <justinbenito1974@gmail.com>
2023-08-28 18:17:28 +0530

    final

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,6 +5,11 @@ import SectionHeading from './section-heading'
 import {delay, motion} from 'framer-motion'
 
 export default function Contact() {
+
+const handleSubmit=()=>{
+  alert("Thanks for Reaching out to me, I'll get back to you shortly")
+}
+
   return (
    <section id='contact' className='mt-28'>
    <SectionHeading>Contact Me</SectionHeading>
@@ -28,6 +33,7 @@ export default function Contact() {
                  animate={{ opacity: 1, scale: 1 }}
                  transition={{ duration: 0.5 }}
               type="submit"
+              onSubmit={handleSubmit}
              className="inline-flex focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition items-center hover:bg-white bg-black  text-white hover:text-black focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 border-white border-2 focus:ring-primary-300">
                 Contact Me
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
